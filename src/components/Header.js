923784d947eb060b7ccafdff69ce369f42225b31
Doug <douglas_greenwood@hotmail.com>
2021-09-07 12:20:28 +0100

    Added demo API endpoints

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,62 @@
-import React, { Component } from "react";
-import MenuLinksData from "../data/MenuLinks.json";
+import React, { useState, useEffect } from "react";
+// import menuLinksData from './data/menu_links.json'
 
-class Header extends Component {
-  render() {
-    return (
-      <header id="intro">
-        <article className="fullheight">
-          <div className="hgroup">
-            <h1>Landon Hotel</h1>
-            <h2>West London</h2>
-            <p>
-              <a href="#welcome">
-                <img
-                  src="https://landonhotel.com/images/misc/arrow.png"
-                  alt="down arrow"
-                />
-              </a>
-            </p>
-          </div>
-        </article>
+const Header = () => {
+  const [menuLinksData, setMenuLinksData] = useState([]);
 
-        <nav id="nav">
-          <div className="navbar">
-            <div className="brand">
-              <a href="#welcome">
-                Landon <span>Hotel</span>
-              </a>
-            </div>
-            <ul>
-              {MenuLinksData.map((item, i) => (
-                <li key={i}>
-                  <a className={`icon ${item.class}`} href={item.href}>
-                    <span>{item.text}</span>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </nav>
-      </header>
+  const loadMenuLinksData = async () => {
+    // Query the API Gateway
+    const resp = await fetch(
+      "https://37yhk6o9fi.execute-api.eu-west-2.amazonaws.com/Production/menulinks"
     );
-  }
-}
+    let jsonData = await resp.json();
+
+    // Assign response data to our state variable
+    setMenuLinksData(jsonData);
+  };
+
+  useEffect(() => {
+    // Load the menu links data from the API Gateway
+    loadMenuLinksData();
+  }, []);
+
+  return (
+    <header id="intro">
+      <article className="fullheight">
+        <div className="hgroup">
+          <h1>Landon Hotel</h1>
+          <h2>West London</h2>
+          <p>
+            <a href="#welcome">
+              <img
+                src="https://landonhotel.com/images/misc/arrow.png"
+                alt="down arrow"
+              />
+            </a>
+          </p>
+        </div>
+      </article>
+
+      <nav id="nav">
+        <div className="navbar">
+          <div className="brand">
+            <a href="#welcome">
+              Landon <span>Hotel</span>
+            </a>
+          </div>
+          <ul>
+            {menuLinksData.map((link) => (
+              <li>
+                <a className={`icon ${link.class}`} href={link.href}>
+                  <span>{link.text}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,42 +1,54 @@
-import React, { Component } from "react";
-import WelcomeImagesData from "../data/Welcome-Images.json";
+import React, { useState, useEffect } from "react";
+//import WelcomeImagesData from "../data/Welcome-Images.json";
 
-class Welcome extends Component {
-  render() {
-    return (
-      <div className="scene" id="welcome">
-        <article className="content">
-          <div className="gallery">
-            {WelcomeImagesData.map((item, i) =>
-              item.class ? (
-                <img
-                  key={i}
-                  src={item.src}
-                  alt={item.alt}
-                  className={item.class}
-                />
-              ) : (
-                <img key={i} src={item.src} alt={item.alt} />
-              )
-            )}
-          </div>
-          <h1>Welcome to the Landon&nbsp;Hotel</h1>
-          <p>
-            The original Landon perseveres after 50 years in the heart of West
-            London. The West End neighborhood has something for everyone—from
-            theater to dining to historic sights. And the not-to-miss Rooftop
-            Cafe is a great place for travelers and locals to engage over
-            drinks, food, and good&nbsp;conversation. &nbsp;To learn more about
-            the Landon Hotel in the West End, browse our website and{" "}
-            <a href="files/landon_information_sheet_London.pdf">
-              download our handy information sheet
-            </a>
-            .
-          </p>
-        </article>
-      </div>
+const Welcome = () => {
+  const [WelcomeImagesData, setWelcomeImagesData] = useState([]);
+
+  const loadWelcomeImagesData = async () => {
+    const resp = await fetch(
+      "https://37yhk6o9fi.execute-api.eu-west-2.amazonaws.com/Production/galleryimages"
     );
-  }
-}
+    let jsonData = await resp.json();
+    setWelcomeImagesData(jsonData);
+  };
+
+  useEffect(() => {
+    loadWelcomeImagesData();
+  }, []);
+
+  return (
+    <div className="scene" id="welcome">
+      <article className="content">
+        <div className="gallery">
+          {WelcomeImagesData.map((item, i) =>
+            item.class ? (
+              <img
+                key={i}
+                src={item.src}
+                alt={item.alt}
+                className={item.class}
+              />
+            ) : (
+              <img key={i} src={item.src} alt={item.alt} />
+            )
+          )}
+        </div>
+        <h1>Welcome to the Landon&nbsp;Hotel</h1>
+        <p>
+          The original Landon perseveres after 50 years in the heart of West
+          London. The West End neighborhood has something for everyone—from
+          theater to dining to historic sights. And the not-to-miss Rooftop Cafe
+          is a great place for travelers and locals to engage over drinks, food,
+          and good&nbsp;conversation. &nbsp;To learn more about the Landon Hotel
+          in the West End, browse our website and{" "}
+          <a href="files/landon_information_sheet_London.pdf">
+            download our handy information sheet
+          </a>
+          .
+        </p>
+      </article>
+    </div>
+  );
+};
 
 export default Welcome;
